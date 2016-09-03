3863c37f1908c8602dba957a1b53e0f751863825
Edmund Li <edmund.xz.lee@gmail.com>
2016-09-02 21:16:49 -0700

    settings singleton

---
+++ settings.js
@@ -0,0 +1,46 @@
+(() => {
+  if (window.GitHubNotifications === undefined) {
+    window.GitHubNotifications = {};
+  }
+
+  let GitHubNotifications = window.GitHubNotifications;
+
+  const Settings = (() => {
+    var instance;
+
+    function init() {
+      const defaults = {
+        baseUrl: 'https://api.github.com/notifications',
+        accessToken: '',
+        onlyParticipating: true,
+        interval: 1
+      };
+
+      return {
+        get: name => {
+          const isSet = localStorage.hasOwnProperty(name);
+          let value = isSet ? localStorage.getItem(name) : defaults[name];
+          if (value === 'true' || value === 'false') {
+            return value === 'true';
+          } else {
+            return value;
+          }
+        },
+        set:    localStorage.setItem.bind(localStorage),
+        remove: localStorage.removeItem.bind(localStorage),
+        reset:  localStorage.clear.bind(localStorage)
+      };
+    }
+
+    return {
+      getInstance: () => {
+        if (!instance) {
+          instance = init();
+        }
+        return instance;
+      }
+    };
+  })();
+
+  GitHubNotifications.Settings = Settings.getInstance();
+})();
