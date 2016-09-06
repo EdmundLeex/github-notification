3589b4b5a462d6c2498118f54d5e828d358f17a0
Edmund Li <edmund.xz.lee@gmail.com>
2016-09-06 15:35:25 -0700

    added publisher

---
+++ extension/scripts/publisher.js
@@ -0,0 +1,50 @@
+;(() => {
+  'use strict';
+
+  if (window.GitHubNotifications === undefined) {
+    window.GitHubNotifications = {};
+  }
+
+  let GitHubNotifications = window.GitHubNotifications;
+
+  const Publisher = (() => {
+    let instance;
+    let subscribers = {};
+
+    function init() {
+      function on(evt, fn) {
+        if (typeof fn !== 'function') {
+          throw new TypeError('You can only subscribe a function');
+        }
+
+        if (subscribers[evt] === undefined) subscribers[evt] = [];
+        subscribers[evt].push(fn);
+
+        return true;
+      }
+
+      function publish(evt) {
+        const events = subscribers[evt];
+        events.forEach(fn => {
+          fn.call(this);
+        });
+      }
+
+      return {
+        on:      on,
+        publish: publish
+      };
+    }
+
+    return {
+      getInstance: () => {
+        if (!instance) {
+          instance = init();
+        }
+        return instance;
+      }
+    };
+  })();
+
+  GitHubNotifications.Publisher = Publisher.getInstance();
+})();
